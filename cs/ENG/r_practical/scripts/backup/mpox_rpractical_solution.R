7973e6c5a8214a5dcd024259b943aff9fb186825
amateo250 <amateo250@hotmail.com>
2024-07-18 15:29:35 +0200

    Update R practical

--- cs/ENG/r_practical/scripts/backup/mpox_rpractical_solution.R
+++ cs/ENG/r_practical/scripts/backup/mpox_rpractical_solution.R
@@ -1,27 +1,24 @@
----
-title: "Mpox: Importing, cleaning and describing data"
-author: "Xanthi Andrianou, Gianfranco Spiteri (ECDC EI Group). Adapted by Alberto Mateo Urdiales"
-site: "bookdown::bookdown_site"
-output:
-  bookdown::gitbook:
-    split_by: none
-    self_contained: true
-    code_folding: hide
-editor_options: 
-  chunk_output_type: console
----
 
 
-```{r Mpox-Rpractical-1, include=FALSE}
+# Step 1: Getting ready for importing the data ----------------------------
+
+
+## Step 1.2 Define R language ----------------------------------------------
+
 # To see your language locale
 Sys.getlocale()
 
 # To change it into English
 Sys.setlocale("LC_ALL", "English")
 
+
+
+## Step 1.3 Install/load packages ------------------------------------------
+
 # Ensures the package "pacman" is installed
 if (!require("pacman")) {
-     install.packages("pacman") }
+  install.packages("pacman") }
+
 
 # install (if necessary) from CRAN and load packages to be used
 pacman::p_load(
@@ -37,31 +34,24 @@ pacman::p_load(
 
 
 
+# Step 2: Import and explore the data -------------------------------------
 
-```
+
+## Step 2.1: Import the different data frames ------------------------------
 
 
-# Importing
+# Case-based data
+cb_data_raw_csv <- import("data/E_pox_case_based_data.csv")
+cb_data_raw_json <- import("data/E_pox_case_based_data.json")
+cb_data_raw_xlsx <- import("data/E_pox_case_based_data.xlsx")
 
-## Case-based data
+# Aggregated data
+agg_data_raw_csv <- import("data/E_pox_aggregated_data.csv")
+agg_data_raw_json <- import("data/E_pox_aggregated_data.json")
+agg_data_raw_xlsx <- import("data/E_pox_aggregated_data.xlsx")
 
-```{r Mpox-Rpractical-2 }
-cb_data_raw_csv <- import("data/raw/E_pox_case_based_data.csv")
-cb_data_raw_json <- import("data/raw/E_pox_case_based_data.json")
-cb_data_raw_xlsx <- import("data/raw/E_pox_case_based_data.xlsx")
-```
 
-## Aggregated data
-
-```{r Mpox-Rpractical-3 }
-agg_data_raw_csv <- import("data/raw/E_pox_aggregated_data.csv")
-agg_data_raw_json <- import("data/raw/E_pox_aggregated_data.json")
-agg_data_raw_xlsx <- import("data/raw/E_pox_aggregated_data.xlsx")
-
-```
-
-```{r}
-# Explore the different case-based data frames
+## Step 2.2: Explore the different data frames -----------------------------
 
 skim(cb_data_raw_csv)
 skim(cb_data_raw_json)
@@ -87,14 +77,12 @@ skim(agg_data_raw_xlsx)
 # Remove json and xlsx files as they are exactly the same as the csv ones. Within rm() we ask for the objects containing the pattern "json" or "xlsx" to be removed from the environment
 rm(list = ls(pattern = "json|xlsx"))
 
-```
+
+# Step 3: Cleaning the data -----------------------------------------------
 
 
-# Cleaning
+## Step 3.1: Clean the case-based data -------------------------------------
 
-## Case-based data
-
-```{r Mpox-Rpractical-4 }
 # Create a new object called cb_data which is the clean version of the raw data, applying the cleaning functions
 
 
@@ -105,13 +93,13 @@ cb_data <- cb_data_raw_csv %>%
   #(Note: after this point all column names have changed)
   
   mutate(date_of_notification = ymd(date_of_notification)) %>%  #transform ONE column into date
-
+  
   mutate(across(starts_with("date"), 
                 .fns = ~ ymd(.x))) %>%  #transforms ALL columns starting with "date" into dates
   
   mutate(week_date = floor_date(date_of_notification, # create week column with Monday start
-                              unit = "week",
-                              week_start = "Monday")) %>% 
+                                unit = "week",
+                                week_start = "Monday")) %>% 
   
   mutate(across(where(is.character), 
                 .fns = ~ ifelse(.x == "", NA, .x)))  %>% #transforms empty cells into NA across all character columns
@@ -122,7 +110,7 @@ cb_data <- cb_data_raw_csv %>%
                          "O" = "Other",
                          "UNK" = "Unknown")) %>%
   
-    
+  
   mutate(across(where(is.character), 
                 .fns = ~ ifelse(.x == "UNK", "Unknown", .x)))  %>% #transforms UNK to Unknown across all character columns
   
@@ -162,13 +150,11 @@ tabyl(cb_data, sexual_orientation)
 tabyl(cb_data, week_date)
 
 tabyl(cb_data, age_group)
-```
 
 
 
-## Aggregated data
+## Step 3.2: Clean the aggregated data -------------------------------------
 
-```{r Mpox-Rpractical-5 }
 # Check class of date of reporting column
 
 class(agg_data_raw_csv$DateRep) #It is a date, so we do not need to change its class
@@ -182,21 +168,21 @@ agg_data <- agg_data_raw_csv %>%
   #(Note: after this point all column names have changed)
   
   mutate(week_date = floor_date(date_rep, # create week column with Monday start
-                              unit = "week",
-                              week_start = "Monday")) 
+                                unit = "week",
+                                week_start = "Monday"))
 
-```
 
-# Basic descriptives of the case-based data
 
-## Summary of the characteristics of the cases (tables and graphs)
 
-### Summary by country
+# Step 4: Basic descriptives ----------------------------------------------
 
-```{r Mpox-Rpractical-6 }
+
+## Step 4.1: Table my place (country) --------------------------------------
+
+
 # Create an object with the table
 cb_country_table <- cb_data %>%
-
+  
   select(country) %>% #select the column that we want to use in the table
   
   gtsummary::tbl_summary() # create the table
@@ -204,13 +190,9 @@ cb_country_table <- cb_data %>%
 # Ask R to print the table
 cb_country_table
 
-```
 
-### Time
+## Step 4.2: Epicurve by week of notification (overall) --------------------
 
-#### Epicurves (overall and by country)
-
-```{r Mpox-Rpractical-8, echo=TRUE, fig.height=7, fig.width=9}
 epicurve_epox <- ggplot(data = cb_data,          #data to be used
                         aes(x = week_date)) +    #with geom_histogram() you only need to assign the x axis
   
@@ -220,7 +202,7 @@ epicurve_epox <- ggplot(data = cb_data,          #data to be used
                  alpha=0.8) +                    #transparency of the bins
   
   scale_x_date(breaks = "2 weeks") +             #set the x axis labels to two week intervals
-
+  
   
   labs(title="Mpox cases reported in 2022") +  #add a title
   
@@ -229,26 +211,25 @@ epicurve_epox <- ggplot(data = cb_data,          #data to be used
   theme(axis.text = element_text(size=9),       #define the font size of the axis text
         axis.title = element_blank(),           #remove the titles of the x and y axis 
         axis.text.x = element_text(angle=90))   #rotate the x axis text
-           
-  
+
+
 epicurve_epox
 
-```
 
+## Step 4.3: Epicurve by week of notification (by country) -----------------
 
-```{r Mpox-Rpractical-9,  echo=FALSE, fig.height=7, fig.width=9}
 epicurve_epox_country <- ggplot(data = cb_data,  #data to be used
-                        aes(x = week_date,       
-                            fill = country)) +   #now the fill needs to be inside aes()  
+                                aes(x = week_date,       
+                                    fill = country)) +   #now the fill needs to be inside aes()  
   
   geom_histogram(binwidth = 7,                   #binwidth 7 ensures that the width represents 7 days
                  color="white",                  #outline colour of the bins
                  alpha=0.8) +                    #transparency of the bins
   
   scale_fill_viridis_d() +                       #we change the predefined colours
-
+  
   scale_x_date(breaks = "2 weeks") +             #set the x axis labels to two week intervals
-
+  
   
   labs(title="Mpox cases reported by country in 2022") +  #add a title
   
@@ -259,99 +240,31 @@ epicurve_epox_country <- ggplot(data = cb_data,  #data to be used
         axis.title = element_blank(),           #remove the titles of the x and y axis 
         axis.text.x = element_text(angle=90),   #rotate the x axis text
         legend.title = element_blank())         #remove title of legend
-           
-  
+
+
 epicurve_epox_country
 
-```
-
-#### Heat plot with cases notified by country
-
-```{r}
-hp_epox <- cb_data %>% #we first group the data by country and week of notification
-  
-  group_by(country, week_date) %>% 
-  
-  summarise(n_cases = n(), .groups = "drop") %>% 
-
-  #now we can use the pipe to directly plot the resulting data from the grouping
-  
-  ggplot(aes(x = week_date,
-           y = country,           #we want the countries to be in the y axis
-           fill = n_cases)) +     #the colour of the tiles should depend on the number of cases
-  
-  geom_tile(colour = "black") +   #this is the outline colour of each tile
-  
-  scale_fill_gradient(            #here we define the colours we want to use in the gradient
-    low = "lightgreen",
-    high = "red") +
-  
-  scale_x_date(breaks = "2 weeks") +             #set the x axis labels to two week intervals
-  
-  labs(
-    title= "Mpox cases by country and week of notification",
-    fill = "Number of cases"                               
-  ) +
-  
-  theme_bw() +
-  
-  theme(legend.position = "bottom",             #legend position to the bottom
-        axis.text = element_text(size=9),       #define the font size of the axis
-        axis.title = element_blank(),           #remove the titles of the x and y 
-        axis.text.x = element_text(angle=90))   #rotate the x axis text
-    
-hp_epox 
-```
 
 
-#### Demographic characteristics
+## Step 4.4: Demographic characteristics -----------------------------------
 
-```{r}
 # Explore gender and age group columns
 tabyl(cb_data, gender)
 tabyl(cb_data, age_group)
 
-#Creating the age and gender pyramid
+# Table with sexual orientation 
 
-agegender_pyramid <- cb_data %>%
+tab_sor <- cb_data %>% 
   
-  filter(gender=="Female"|gender=="Male") %>% #we first filter the data to keep cases with gender male or female
-  
-  drop_na(age_group) %>%  #we now remove those with missing age
-  
-  # Create age gender pyramid
-  age_pyramid(
-  
-  age_group = "age_group",    # Specify column containing age groups
-  
-  split_by = "gender",        # Splitting by Gender
-  
-  show_midpoint = FALSE) +    # don't show midpoint on the graph:
-  
-  theme_bw() +
-  
-  theme(legend.position = "bottom") +
-  
-  labs(x="",                  #in age pyramid, the axis are flipped
-       y= "Number of cases",
-       fill = "Gender")       # so that gender has upper G
-
-
-agegender_pyramid
-
-
-#Table by sexual orientation
-
-table_sex_or <- cb_data %>%
   select(sexual_orientation) %>% 
-  tbl_summary()
-table_sex_or
-```
+  
+  tbl_summary(label = list(sexual_orientation ~ "Sexual Orientation")) 
+
+tab_sor
 
 
-#### Clinical characteristics
+## Step 4.5: Clinical characteristics --------------------------------------
 
-```{r}
 # Bar plot with clinical symptoms
 
 bar_clinical <- cb_data %>% 
@@ -405,14 +318,11 @@ tab_hiv_sor <- cb_data %>%
 
 tab_hiv_sor
 
-```
+
+# Step 5: Optional analysis -----------------------------------------------
 
 
-
-####  Delay between date of onset, diagnosis and notification
-
-
-```{r Mpox-Rpractical-10, fig.height=7, fig.width=9}
+## Step 5.1: Delay between date of onset, diagnosis and notification -------
 
 # Estimate delay between onset and diagnosis, and between diagnosis and notification
 
@@ -438,24 +348,24 @@ delay_country
 # Line graph with the different dates 
 
 dates_longer <- cb_data %>% # use the variables of the dates and make a longer dataset. In the pivot_longer() command we select the columns which we want to expand in long format and transform the dataset
-   
+  
   pivot_longer(
     
     cols=starts_with("date_"),         # all columns starting with "date_" will be taken 
-
+    
     names_to = "indicator",            #the names of the columns will be placed in a single column called "indicator"
-
+    
     values_to = "date")                # the values (which are dates in this case) will be placed in a column called "date"
-  
+
 
 dates_longer_week <- dates_longer  %>% 
-
+  
   mutate(week_date = floor_date(date, unit = "week", week_start = "Monday")) %>%  # we create a week column
-    
+  
   group_by(indicator, week_date) %>% 
-    
+  
   summarise(n=n(), .groups="drop") %>%   # we group and summarise to have the number of cases by date type and week
-    
+  
   drop_na(week_date)                     # we drop the cases with no data on dates
 
 
@@ -481,12 +391,10 @@ plot_date_delay <-   ggplot(data = dates_longer_week,
 
 plot_date_delay
 
-```
 
 
-# (Optional). Combine case-based and aggregated data and generate a summary of the information
+## Step 5.2: Compare case-based and aggregated data ------------------------
 
-```{r}
 # Create a data frame with the overall number of cases reported through the aggregated flux
 
 agg_data_country <- agg_data %>% 
@@ -496,7 +404,7 @@ agg_data_country <- agg_data %>%
   filter(date_rep == max(date_rep)) %>% # as we have cumulative data, we keep only the last week (after grouping by country)
   
   select(-date_rep, -week_date) %>%     # remove unnecessary columns
-
+  
   mutate(source = "aggregated")         # we create this column to distinguish the numbers from the case-based flux
 
 
@@ -509,7 +417,7 @@ cb_data_country <- cb_data %>%
   summarise(cases = n(), .groups = "drop") %>% 
   
   mutate(source = "case_based")       # we create this column to distinguish the numbers from the
-  
+
 
 # We append both data frames. Remember this is different from merging
 
@@ -526,10 +434,10 @@ graph_comp <- ggplot(data = total_data,
   geom_col(position = "dodge") +            #position dodge puts bars one next to each other, instead of "stacked"
   
   facet_wrap(~ country, scales = "free_y") +  # this command gives us one graph per country. The argument scales is used to allow each y axis scales to adjust to the data
-
+  
   scale_fill_viridis_d(
     labels = c("Aggregated", "Case-based")  # this function changes the colours, but with the argument "labels" we can change the text of each fill.
-     ) +
+  ) +
   
   
   labs(
@@ -545,5 +453,42 @@ graph_comp <- ggplot(data = total_data,
         axis.ticks.x = element_blank())     # we also remove the ticks for aesthetic purposes
 
 graph_comp
-```
 
+
+
+## Step 5.3: Heat plot with number of cases by country and week of  --------
+
+
+hp_epox <- cb_data %>% #we first group the data by country and week of notification
+  
+  group_by(country, week_date) %>% 
+  
+  summarise(n_cases = n(), .groups = "drop") %>% 
+  
+  #now we can use the pipe to directly plot the resulting data from the grouping
+  
+  ggplot(aes(x = week_date,
+             y = country,           #we want the countries to be in the y axis
+             fill = n_cases)) +     #the colour of the tiles should depend on the number of cases
+  
+  geom_tile(colour = "black") +   #this is the outline colour of each tile
+  
+  scale_fill_gradient(            #here we define the colours we want to use in the gradient
+    low = "lightgreen",
+    high = "red") +
+  
+  scale_x_date(breaks = "2 weeks") +             #set the x axis labels to two week intervals
+  
+  labs(
+    title= "Mpox cases by country and week of notification",
+    fill = "Number of cases"                               
+  ) +
+  
+  theme_bw() +
+  
+  theme(legend.position = "bottom",             #legend position to the bottom
+        axis.text = element_text(size=9),       #define the font size of the axis
+        axis.title = element_blank(),           #remove the titles of the x and y 
+        axis.text.x = element_text(angle=90))   #rotate the x axis text
+
+hp_epox
