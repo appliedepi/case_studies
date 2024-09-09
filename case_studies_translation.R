3317d5bb70f5de4b258604189829fc2497604d51
Luong Nguyen Thanh <ph.ntluong95@gmail.com>
2024-09-09 20:37:50 +0700

    translated 3 EN case studies by DeepL

--- case_studies_translation.R
+++ case_studies_translation.R
@@ -62,64 +62,64 @@ babeldown::deepl_translate_quarto(
 
 
 # # Write a loop to run the translation for each chapter based on chapter_list
-# for (i in 1:length(chapter_list)) {
-#      babeldown::deepl_translate_quarto(
-#           book_path = here::here("pages"),
-#           chapter = chapter_list[i],
-#           force = TRUE,
-#           render = FALSE, # Whether to run babelquarto::render_bool() after translation.
-#           source_lang = "EN",
-#           target_lang = "ES",
-#           formality = "less")
-# }
+for (i in 1:length(chapter_list)) {
+     babeldown::deepl_translate_quarto(
+          book_path = here::here("pages"),
+          chapter = chapter_list[i],
+          force = TRUE,
+          render = FALSE, # Whether to run babelquarto::render_bool() after translation.
+          source_lang = "EN",
+          target_lang = "ES",
+          formality = "more")
+}
 
 # # 4. Review the translation, make a PR and re-render the book
 
 
 
-# UPDATE LANGUAGE TRANSLATION PROTOCOL ------------------------------------------------------------------------
-
-
-diffs <- system("git diff --name-only main...", intern = TRUE) # to include only committed file, use ... after main
-chapters_changed <- diffs[stringr::str_detect(diffs, "\\.qmd$")] # Filter only .qmd files
-
-# 2. Create vector of target languages
-
-deepL_lang = c("FR", "ES", "JA", "PT-PT", "TR", "RU") #, "VN"
-target_lang = c("fr", "es", "jp", "pt", "tr", "ru") #, "vn" 
-
-
-# 3. Create a vector of chapters changed
-# Create a named list where each original chapter filename maps to its new versions with language codes
-chapters_changed_new <- setNames(
-  lapply(chapters_changed, function(chapter) {
-    sapply(target_lang, function(lang) {
-    sub("\\.qmd$", paste0(".", lang, ".qmd"), chapter)
-  })}),
-  chapters_changed)
-
-
-# 4. Write a loop to run the updated translation for each chapter based on chapter_changes_update, noted that the target_lang argument should be also adapted based on target_lang 
-
-for (old_chapter in names(chapters_changed_new)) {
-  # Get the vector of new filenames for the current original chapter
-  new_chapters <- chapters_changed_new[[old_chapter]]
-  
-  # Loop over each new filename and its corresponding language
-  for (idx in seq_along(new_chapters)) {
-    new_chapter <- new_chapters[idx]
-    lang <- deepL_lang[idx]
-
-    try({
-    babeldown::deepl_update(
-      path = here::here(old_chapter),
-      out_path = here::here(new_chapter),
-      source_lang = "EN",
-      target_lang = lang,
-      formality = "less",
-      yaml_fields = NULL)
-    }, silent = FALSE)    
-  }
-}
+# # UPDATE LANGUAGE TRANSLATION PROTOCOL ------------------------------------------------------------------------
+# 
+# 
+# diffs <- system("git diff --name-only main...", intern = TRUE) # to include only committed file, use ... after main
+# chapters_changed <- diffs[stringr::str_detect(diffs, "\\.qmd$")] # Filter only .qmd files
+# 
+# # 2. Create vector of target languages
+# 
+# deepL_lang = c("FR", "ES", "JA", "PT-PT", "TR", "RU") #, "VN"
+# target_lang = c("fr", "es", "jp", "pt", "tr", "ru") #, "vn" 
+# 
+# 
+# # 3. Create a vector of chapters changed
+# # Create a named list where each original chapter filename maps to its new versions with language codes
+# chapters_changed_new <- setNames(
+#   lapply(chapters_changed, function(chapter) {
+#     sapply(target_lang, function(lang) {
+#     sub("\\.qmd$", paste0(".", lang, ".qmd"), chapter)
+#   })}),
+#   chapters_changed)
+# 
+# 
+# # 4. Write a loop to run the updated translation for each chapter based on chapter_changes_update, noted that the target_lang argument should be also adapted based on target_lang 
+# 
+# for (old_chapter in names(chapters_changed_new)) {
+#   # Get the vector of new filenames for the current original chapter
+#   new_chapters <- chapters_changed_new[[old_chapter]]
+#   
+#   # Loop over each new filename and its corresponding language
+#   for (idx in seq_along(new_chapters)) {
+#     new_chapter <- new_chapters[idx]
+#     lang <- deepL_lang[idx]
+# 
+#     try({
+#     babeldown::deepl_update(
+#       path = here::here(old_chapter),
+#       out_path = here::here(new_chapter),
+#       source_lang = "EN",
+#       target_lang = lang,
+#       formality = "less",
+#       yaml_fields = NULL)
+#     }, silent = FALSE)    
+#   }
+# }
 
 
